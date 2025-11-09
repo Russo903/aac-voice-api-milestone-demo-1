d2aa07c5c96d9cf7647e787847121e04af1e1a33
Michael Colbert <colbert9502@gmail.com>
2025-11-08 23:26:26 -0500

    added updated stuff

--- demofiles/onlineMode.js
+++ demofiles/onlineMode.js
@@ -1,6 +1,7 @@
             else {
                 await voiceApi.current.initiate({//d4s
                     mode: 'online',
+                    modelUrl: 'http://localhost:8000',
                     useSpeakerSeparation: useSeparation
                 });//d4e
                 const sepText = useSeparation ? ' with speaker separation' : ' (single speaker)';
